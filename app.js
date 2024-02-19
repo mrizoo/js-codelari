eb81e61ea12a3ab4fd7c0bf7bc5e89b64f88f043
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-02-19 22:56:25 +0500

    10 dars

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log(1);
+console.log(2);
+
+
+let ism = 'Muhammadrizo'
+let yosh = '18'
+console.log(ism,yosh);
+
+yosh = 19
+
+console.log(yosh);
+
+const familya = `Ibrohimov`
+console.log(familya);
+
+var place = `Farg'ona`
+place = `Farg'ona shaxri`
+console.log(place);
+
+let Name = `Muhammadrizo`
+let Age = 18
+let = 19
+
+console.log(`Whusssap my little bro + !`);
+
+let email = `pulatovvmukhammadrizo@gmail.com`
+console.log(email);
+
+//string concatenation
+let firstName = `Muhammadrizo`
+let lastName = `Pulatovv`
+let fullNAme =  firstName  + ` `  +  lastName
+console.log(fullNAme);
+
+//get charactes 
+console.log(fullNAme[8]);
+
+//string lengh => uzunlik 
+console.log(fullNAme.length);6
+
+
+//string metdodos
+
+console.log(fullNAme.toLowerCase());
+
+let radius = 10
+const pi = 3.14
+
+console.log(radius, pi);
+
+//moth operators + - / . . . 
+
+//conole.log(10/2)
+
+// let result = 12 % 5
+// console.log(result);
+
+let result = pi * radius ** 2
+
+console.log(result);
